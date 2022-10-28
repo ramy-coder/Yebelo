9d4b9596d18c786ed02ee29de98bf4033b206253
Ramya <ramyaperumaal@gmail.com>
2022-10-28 17:18:48 +0530

    next

--- Tokenslab.sol
+++ Tokenslab.sol
@@ -1,16 +1,35 @@
     //SPDX-License-Identifier: MIT
-    pragma solidity ^0.8.9;
+    pragma solidity ^0.8.0;
 
     contract Tokenslab {
         uint8 currentSlab;
-        uint8 slabLimits[5];
+        uint32[5] slabLimits;
+        uint64[5] slabValues;
 
         constructor()
         {
             slabLimits = [100,200,300,400,500];
             currentSlab = 4;
+            slabValues = [0,0,0,0,0];
         }
 
-        function 
+        function tokenDeposit(uint64 noofTokens) public
+        {
+            slabValues[currentSlab] += noofTokens;
+            if(slabValues[currentSlab] > slabLimits[currentSlab])
+            {
+                overflowHandler();
+            }
+        }
+
+        function overflowHandler() private 
+        {
+            uint64 overFlow;
+            overFlow = slabValues[currentSlab] - slabLimits[currentSlab];
+            slabValues[currentSlab] = slabLimits[currentSlab];
+            currentSlab = currentSlab - 1;
+
+            
+        } 
 
     }
