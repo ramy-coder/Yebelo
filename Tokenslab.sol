7baa0a0c6386ccce7f445b8f0d57a539d5aca6ff
Ramya <ramyaperumaal@gmail.com>
2022-10-28 16:56:06 +0530

    Second Commit

---
+++ Tokenslab.sol
@@ -0,0 +1,16 @@
+    //SPDX-License-Identifier: MIT
+    pragma solidity ^0.8.9;
+
+    contract Tokenslab {
+        uint8 currentSlab;
+        uint8 slabLimits[5];
+
+        constructor()
+        {
+            slabLimits = [100,200,300,400,500];
+            currentSlab = 4;
+        }
+
+        function 
+
+    }
